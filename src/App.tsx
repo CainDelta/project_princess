4782a8eeeb064ed5b3326140b843acad04386b0c
Architect <architect@omega.local>
2024-02-12 23:25:05 +0000

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ export default function Page() {
       {yesPressed ? (
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
+          <div className="my-4 text-4xl font-bold">WOOOOOO!!! You're the best!! ;))</div>
         </>
       ) : (
         <>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
+import { defineConfig, Plugin } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/will-you-be-my-valentine",
+  base: "/project_princess",
   plugins: [react()],
 });
