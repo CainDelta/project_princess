c16dde4be82b37e5c8fe86114c1050b04142353b
delta <afmubaiwa@gmail.com>
2024-02-13 17:54:55 +0000

    mistake

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ export default function Page() {
           //   className="h-[200px]"
           //   src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
           // />
-          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
+          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif"
           <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
           <div className="flex items-center">
             <button
