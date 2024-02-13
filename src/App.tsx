c7a9bd0bc5221b689d4eaa9874a302e2d8516822
delta <afmubaiwa@gmail.com>
2024-02-13 17:59:10 +0000

    changed typo

--- src/App.tsx
+++ src/App.tsx
@@ -44,11 +44,8 @@ export default function Page() {
         </>
       ) : (
         <>
-          // <img
-          //   className="h-[200px]"
-          //   src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
-          // />
-          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif"
+    
+          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
           <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
           <div className="flex items-center">
             <button
